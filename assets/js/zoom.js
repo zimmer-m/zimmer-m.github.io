d6faa5d20522dc8601f98adceeca050394c9b45f
Max Zimmer <m.zimmer@mailbox.org>
2025-04-13 12:14:52 +0200

    Zooming works, forgot file

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -3,5 +3,7 @@ $(document).ready(function() {
   medium_zoom = mediumZoom('[data-zoomable]', {
     background: getComputedStyle(document.documentElement)
         .getPropertyValue('--global-bg-color') + 'ee',  // + 'ee' for trasparency.
+    margin: 48,
+    zoom: 0.8  // Zoom to 80% of the original full-zoom size
   })
 });
