78b1a00c3b64b2333faebd3a9523660d953581f8
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2025-06-12 22:09:46 +0000

    added oral to neural discovery [ci skip]

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -1,9 +1,1 @@
-// Initialize medium zoom.
-$(document).ready(function() {
-  medium_zoom = mediumZoom('[data-zoomable]', {
-    background: getComputedStyle(document.documentElement)
-        .getPropertyValue('--global-bg-color') + 'ee',  // + 'ee' for trasparency.
-    margin: 48,
-    zoom: 0.8  // Zoom to 80% of the original full-zoom size
-  })
-});
+$(document).ready(function(){medium_zoom=mediumZoom("[data-zoomable]",{background:getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color")+"ee",margin:48,zoom:.8})});
